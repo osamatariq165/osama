678694c8db2d95de315c08d9820ca7992b0dc4b8
osamatariq165 <osama.tariq@tkxel.com>
2023-08-04 17:01:09 +0500

    added image and also services section

--- js/main.js
+++ js/main.js
@@ -308,3 +308,10 @@
 
 
 }());
+
+function sendMessage() {
+	console.log('clicked');
+	var x = document.getElementById("snackbar");
+  x.className = "show";
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
